36b055ae35268d8eb7d8c00c4b2b524033b8a8e5
chezyio <chester.yeezx@gmail.com>
2025-04-10 23:21:43 +0200

    build: add okhttp

--- CouchPotato/app/build.gradle.kts
+++ CouchPotato/app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
