a70bb79dbeb5fbad187c3f664c12cce1719dff84
cshhua <chooshihua@gmail.com>
2025-05-09 15:06:31 +0200

    added WorkManager implementation for fetching Popular & Top movies
    Added data persistence for Popular & Top Rated movies when internet is down with Room and WorkManager

--- CouchPotato/app/src/main/java/com/m7019e/couchpotato/MovieEntity.kt
+++ CouchPotato/app/src/main/java/com/m7019e/couchpotato/MovieEntity.kt
@@ -21,3 +21,40 @@ data class MovieEntity(
     val vote_average: Double,
     val vote_count: Int
 )
+
+//for popular & top rated movies
+@Entity(tableName = "Popular")
+data class PopularMovieEntity(
+    @PrimaryKey val id: Int,
+    val adult: Boolean,
+    val backdrop_path: String?,
+    val genres: List<Genre>,
+    val original_language: String,
+    val original_title: String,
+    val overview: String?,
+    val popularity: Double,
+    val poster_path: String?,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
+)
+
+@Entity(tableName = "TopRated")
+data class TopRatedMovieEntity(
+    @PrimaryKey val id: Int,
+    val adult: Boolean,
+    val backdrop_path: String?,
+    val genres: List<Genre>,
+    val original_language: String,
+    val original_title: String,
+    val overview: String?,
+    val popularity: Double,
+    val poster_path: String?,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
+)
